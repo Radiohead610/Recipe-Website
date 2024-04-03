6c34d9f50bd96ce5b6039b83ef918e594a39894d
Radiohead610 <147007809+Radiohead610@users.noreply.github.com>
2024-04-03 15:55:31 -0400

    Add images.

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 
 function Popular() {
@@ -20,22 +21,37 @@ function Popular() {
         const data = await api.json();
         //once it's been fetched, it needs to be saved somewhere
         setPopular(data.recipes);
-        console.log(data);
+        console.log(data.recipes);
     }
-
-
 
   return (
     <div> 
         {popular.map((recipe) => {
             return(
-                <div key={recipe.id}>
-                    <p>{recipe.title}</p>
-                </div>
+              <Wrapper>
+                <h3>Popular Picks</h3> 
+                {popular.map((recipe) => {
+                    return(
+                        <Card>
+                            <p>{recipe.title}</p>
+                            <img src={recipe.image} alt={recipe.title}/>
+                        </Card>
+                    );
+                })}
+              </Wrapper>
             );
         })}
     </div>
   )
 }
 
+const Wrapper = styled.div`
+    margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+    min-height: 25rem;
+    border-radius: 2rem;
+    overflow: hidden;
+`;
 export default Popular
